c4cc12b85cfb4c9a06528f8f977ad482aa2d6891
MajorTom97 <cesargalileo1969@gmail.com>
2021-12-16 14:24:38 -0600

    Kurz Endes Kommit

--- orders/urls.py
+++ orders/urls.py
@@ -6,5 +6,8 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('', views.index, name='index'),
     path("register/", views.register, name="register"),
-    path('menu/', views.Menu, name="menu")
+    path('menu/', views.Menu, name="menu"),
+    path('cart/', views.cart, name="cart"),
+    path('history/', views.history, name="history"),
+    path('buy/', views.buy, name="buy")
 ]
--- orders/views.py
+++ orders/views.py
@@ -8,7 +8,7 @@ from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
-from orders.models import RegularPizza, SicilianPizza, Toppings, Subs, Pastas, Salads, Extras, Category, DinnerPlatters
+from orders.models import Order, OrderDetail, RegularPizza, SicilianPizza, Toppings, Subs, Pastas, Salads, Extras, Category, DinnerPlatters
 from .forms import  UserRegisterForm
 
 # Create your views here.
@@ -40,29 +40,69 @@ def register(request):
 # Categories 
 
 def Menu(request):
-    reg_pizza = RegularPizza.objects.all()
-    sici_pizza = SicilianPizza.objects.all()
-    toppings = Toppings.objects.all()
+    if request.method == 'GET':
+        
+        reg_pizza = RegularPizza.objects.all()
+        sici_pizza = SicilianPizza.objects.all()
+        toppings = Toppings.objects.all()
 
-    subs = Subs.objects.all()
-    extras = Extras.objects.all()
+        subs = Subs.objects.all()
+        extras = Extras.objects.all()
 
-    pasta = Pastas.objects.all()
+        pasta = Pastas.objects.all()
 
-    salads = Salads.objects.all()
+        salads = Salads.objects.all()
 
-    platters = DinnerPlatters.objects.all()
+        platters = DinnerPlatters.objects.all()
 
-    context = {
-        "reg_pizzas":reg_pizza, #
-        "sici_pizzas": sici_pizza, #
-        "toppings": toppings,
-        "subs": subs, #
-        "extras": extras,
-        "pastas":pasta,
-        "salads":salads,
-        "platters":platters, #
+        context = {
+            "reg_pizzas":reg_pizza, #
+            "sici_pizzas": sici_pizza, #
+            "toppings": toppings,
+            "subs": subs, #
+            "extras": extras,
+            "pastas":pasta,
+            "salads":salads,
+            "platters":platters, #
 
-    }
+        }
 
-    return render(request,"menu.html", context)
+        return render(request,"menu.html", context)
+    else:
+        product = request.POST.get("product")
+        quantity = int(request.POST.get("quantity"))
+        price = float(request.POST.get("price"))
+        toppings = request.POST.getlist("toppings")
+        order = OrderPending(request.user)
+        toppingObject=[]
+
+        for topping in toppings:
+            toppingObject.append(Toppings.objects.get(pk=topping))
+        detailOr = OrderDetail.objects.create(order=order, product=product, price=price, quantity=quantity, subtotal=(float(quantity)*price))
+        if toppings:
+            for topping in toppingObject:
+                detailOr.toppings.add(topping)
+        
+        order.total = float(order.total) + float(detailOr.subtotal)
+        order.save()
+
+        print(request.POST)
+        return redirect("menu")
+
+def OrderPending(user):
+    order = Order.objects.filter(user=user, pending=True).first()
+    if not order:
+        order = Order.objects.create(user=user, total=0)
+    return order
+
+def cart(request):
+    return render(request,"cart.html", {"orders": [OrderPending(request.user), ]})
+
+def history(request):
+    return render(request,"cart.html", {"orders": Order.objects.filter(user = request.user).all()})
+
+def buy(request):
+    order = OrderPending(request.user)
+    order.pending = False
+    order.save()
+    return redirect("menu")
